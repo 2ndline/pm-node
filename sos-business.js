92a456afd779bc48d0332bbe58992619ebd2c1fc
2ndline <bob.murrell@gmail.com>
2023-03-07 13:01:04 -0600

    1 file modified

--- sos-business.js
+++ sos-business.js
@@ -1,22 +1,24 @@
 // run `node index.js` in the terminal
 
-const axios = require('axios');
+const axios = require('axios').default;
 const token = '4phdkyG3O6cdc5NuKQogP4ehrNEfynV4ISD';
 const email = 'electbobmurrell@gmail.com';
-const api = 'https://commercialapi.sos.la.gov/api/';
-const data = {
-  Token: token,
-  EmailAddress: email,
-  EntityName: 'Rouses',
-};
+const api = 'https://commercialapi.sos.la.gov/api/Commercial/Search';
+const searchterm = 'Rouses';
+const requestUrl =
+  api + `?token=${token}&emailaddress=${email}&entityname=${searchterm}`;
+console.log(requestUrl);
 axios
-  .post(api + 'Commercial/Search', data)
-  .then((response) => {
+  .get(requestUrl)
+  .then(function (response) {
+    console.log(response);
     //TODO fix this
     //record document, return id
-    console.log(response);
   })
-  .catch((error) => {
+  .catch(function (error) {
     console.log(error);
+  })
+  .finally(function () {
+    // always executed
   });
 return;
